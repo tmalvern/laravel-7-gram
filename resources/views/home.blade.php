9286561cb4e7f65ba6686c5ec82b9f87f15106a6
tmalvern <tmalvern@gmail.com>
2020-05-04 22:35:51 +1000

    Added the profile functionality

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,15 +7,15 @@
             <img alt="freecodecamp's profile picture" src="https://instagram.fsyd7-1.fna.fbcdn.net/v/t51.2885-19/s150x150/83213956_3360255157381124_5752385570823208960_n.jpg?_nc_ht=instagram.fsyd7-1.fna.fbcdn.net&amp;_nc_ohc=wLIsWO6fABwAX9A5nYn&amp;oh=5b7e92c6a52fc530e357b32fc5d8f8f4&amp;oe=5ED9CD46" class="rounded-circle"> 
         </div>
         <div class="col-9 pt-5">
-            <div><h1>freecodecamp</h1</div>
+            <div><h1>{{ $user->username }}</h1></div>
             <div class="d-flex">
                 <div class="pr-5"><strong>153</strong> posts</div>
                 <div class="pr-5"><strong>23k</strong> followers</div>
                 <div class="pr-5"><strong>212</strong> following</div>
             </div>
-            <div class="pt-5 font-weight-bold">freeCodeCamp.org</div>
-            <div>We're a global community of millions of people learning to code together. We're an open source, donor-supported, 501(c)(3) nonprofit</div>
-            <div><a href="#">www.freecodecamp.org</a></div>
+            <div class="pt-5 font-weight-bold">{{ $user->profile->title }}</div>
+            <div>{{ $user->profile->description }}</div>
+            <div><a href="#">{{ $user->profile->url }}</a></div>
         </div>
     </div>
     <div class="row pt-5">
